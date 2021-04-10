73d62b82c8b7da02a1a49b74093efa4984b86275
Dinkymoo <danielletulp@yahoo.com>
2021-04-10 18:35:46 +0200

    Update superuser method to match override name

--- app/core/models.py
+++ app/core/models.py
@@ -15,7 +15,7 @@ class UserManager(BaseUserManager):
 
         return user
 
-    def create_super_user(self, email, password=None):
+    def create_superuser(self, email, password=None):
         """Creates and saves a new super user"""
         user = self.model(email=self.normalize_email(email),
                           is_staff=True, is_superuser=True)
